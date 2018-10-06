5fe18d9fa6dab182f1c5627cc06770f6d794de75
t-kusakabe <ttmac0704@gmail.com>
2018-10-07 00:30:37 +0900

    create script

--- src/import.ts
+++ src/import.ts
@@ -1,1 +1,28 @@
-console.log('hello, ts');
+import * as dotenv from 'dotenv';
+import * as Slack from 'slack-node';
+import * as request from 'request';
+
+dotenv.config();
+const apiToken = process.env.SLACK_API_TOKEN;
+const slack = new Slack(apiToken);
+
+slack.api('emoji.list', function(err, response) {
+  for(const key in response.emoji) {
+    const url = response.emoji[key]; 
+
+    // exclude alias
+    if(url.match(/alias/)) {
+      continue;
+    }
+
+    // send slash command
+    request.post('https://slack.com/api/chat.command', {
+      form: {
+        token: apiToken,
+        channel: '{channel id}',
+        command: '/reacji-channeler',
+        text: ` :${key}: #{channel name}`
+      }
+    });
+  }
+});
